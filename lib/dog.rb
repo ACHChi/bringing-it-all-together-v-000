86de9152e782bc222f9f4d1cf7eb7d04736d7404
Chao Hsuan Chi <allen0413@gmail.com>
2019-02-26 02:32:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,17 +10,20 @@ attr_reader :id
   end
 
   def self.create_table
-  sql =  <<-SQL
-    CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      breed TEXT
-      )
-  SQL
-  DB[:conn].execute(sql)
-end
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
-
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql) 
+  end
 
 
 
