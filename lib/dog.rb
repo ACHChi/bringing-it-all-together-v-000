a84dc5625721c814e986858e963e291907f6064c
Chao Hsuan Chi <allen0413@gmail.com>
2019-02-26 02:34:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,9 +25,18 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id, name, breed)
+  end
 
-
-
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
 
 
 
@@ -56,11 +65,7 @@ attr_reader :id
     dog
   end
 
-  def self.find_by_id(id)
-    sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
-  end
+
 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
